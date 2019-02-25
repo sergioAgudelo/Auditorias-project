f3c76208fd84543d07deb2d0164a23b6e5acab35
sergio andres agudelo molina <sergio-agudelo@hotmail.es>
2019-02-25 17:31:46 -0500

    .gitignore

--- server/models/pregunta.js
+++ server/models/pregunta.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const preguntaSchema = new Schema({
     numero: { type: String, required: true},
     pregunta: { type: String, required: true },
-    repsuesta: { type: String, required: true }    
+    respuesta: { type: String, required: true }    
 });
 
 module.exports = mongoose.model('Pregunta', preguntaSchema);
